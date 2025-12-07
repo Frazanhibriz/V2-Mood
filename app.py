dd83cf98294af4286c936c02408eee86610625b1
Falah Razan Hibrizi <147212624+Frazanhibriz@users.noreply.github.com>
2025-12-07 23:59:15 +0700

    Adjust column widths in app layout

--- app.py
+++ app.py
@@ -737,7 +737,7 @@ elif st.session_state.step == 3:
     
     st.markdown("<br>", unsafe_allow_html=True)
     
-    col1, col2 = st.columns([1, 1])
+    col1, col2 = st.columns([2, 1])
     with col1:
         if st.button("← Back", key="back_3"):
             st.session_state.step = 2
